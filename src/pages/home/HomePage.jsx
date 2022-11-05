3ebc0127161378f306504730c275d8d55488ec36
Gleb <gleblubiskin@gmail.com>
2022-11-05 21:31:05 +0300

    Установка и настройка tailwindcss

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,5 +1,5 @@
 const HomePage = () => {
-    return <h1>Привет, меня зовут Глеб!</h1>;
+    return <h1 className="text-3xl font-bold underline">Привет, меня зовут Глеб!</h1>;
 };
 
 export default HomePage;
